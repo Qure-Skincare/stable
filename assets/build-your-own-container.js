0544a4bf82730f98e79b38f8867710086377f570
speedup-guru <paul@speedup.guru>
2026-04-27 19:12:55 +0400

    deleted old files

--- assets/build-your-own-container.js
+++
@@ -1,9 +0,0 @@
-var links = document.querySelectorAll("ul.qure_tabs li a");
-links.forEach(function (link) {
-    var id = link.getAttribute("href").replace("#build-your-own-target-", "");
-    var target = document.getElementById("build-your-own-target-" + id);
-    var source = document.getElementById(id);
-    if (source && target) {
-        target.appendChild(source.content.cloneNode(true));
-    }
-});
--- assets/redirect.js
+++ assets/redirect.js
@@ -5,3 +5,7 @@ if (window.location.pathname.includes('/products/micro-infusion-limited-edition-
 if (window.location.pathname.includes('/pages/qure-micro-infusion-landing-page-b')) {
   window.location.replace('/pages/qure-microinfusion-offer');
 }
+
+if (window.location.pathname.includes('/pages/micro-infusion-limited-edition-bundle-1')) {
+  window.location.replace('/pages/qure-microinfusion-offer');
+}
